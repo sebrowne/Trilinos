e6908ecd6fdb30219af00b28e0a1c609b91d18f5
Samuel Browne <sebrown@sandia.gov>
2020-01-21 11:28:28 -0700

    do-cmakes: Add minitrilinos
    Kokkos and SEACAS only.
    
    User Support Ticket(s) or Story Referenced: SPAR-747

---
+++ do-cmake_minitrilinos_cee-cpu_intel_openmp_mpich.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+EXTRA_ARGS=$@
+
+COMPILER_DIR=${COMPILER_ROOT}
+MPI_DIR=${MPI_ROOT}
+BLAS_DIR=${CBLAS_ROOT}
+LAPACK_DIR=${CBLAS_ROOT}
+HDF5_DIR=${HDF5_ROOT}
+NETCDF_DIR=${NETCDF_ROOT}
+PNETCDF_DIR=${PNETCDF_ROOT}
+ZLIB_DIR=/usr/lib64
+CGNS_DIR=${CGNS_ROOT}
+BOOST_DIR=${BOOST_ROOT}
+METIS_DIR=${METIS_ROOT}
+PARMETIS_DIR=${PARMETIS_ROOT}
+SUPERLUDIST_DIR=${SUPERLUDIST_ROOT}
+
+BUILD_TYPE=RELEASE
+BUILD_SUFFIX=opt
+BUILD_C_FLAGS=""
+BUILD_CXX_FLAGS=""
+BUILD_F_FLAGS=""
+BUILD_LINK_FLAGS=""
+
+if   [[ ${1} == 'opt' || ${2} == 'opt' ]]
+then
+  :
+elif [[ ${1} == 'dbg' || ${2} == 'dbg' ]]
+then
+  BUILD_TYPE=DEBUG
+  BUILD_SUFFIX=dbg
+else
+  echo " *** You may specify 'opt' or 'dbg' to this configuration script. Defaulting to 'opt'! ***"
+fi
+
+LINK_SHARED=OFF
+LINK_SUFFIX=static
+
+if   [[ ${1} == 'static' || ${2} == 'static' ]]
+then
+  :
+elif [[ ${1} == 'shared' || ${2} == 'shared' ]]
+then
+  LINK_SHARED=ON
+  LINK_SUFFIX=shared
+else
+  echo " *** You may specify 'static' or 'shared' to this configuration script. Defaulting to 'static'!"
+fi
+
+TRILINOS_HOME=${TRILINOS_REPO_DIR:-$(cd ..; pwd)}
+TRIL_INSTALL_PATH=${TRIL_INSTALL_PATH:-$(cd ..; pwd)}
+TRIL_INSTALL_DIR=${SPARC_ARCH}_${SPARC_COMPILER}_openmp_${SPARC_MPI}_${LINK_SUFFIX}_${BUILD_SUFFIX}
+
+echo " *** Installing in: ${TRIL_INSTALL_PATH}/${TRIL_INSTALL_DIR}"
+sleep 3
+
+rm -f CMakeCache.txt; rm -rf CMakeFiles
+
+cmake \
+   -D CMAKE_VERBOSE_MAKEFILE=FALSE \
+   -D CMAKE_INSTALL_PREFIX:PATH=${TRIL_INSTALL_PATH}/${TRIL_INSTALL_DIR} \
+   -D CMAKE_BUILD_TYPE:STRING=${BUILD_TYPE} \
+   -D BUILD_SHARED_LIBS=${LINK_SHARED} \
+   \
+   -D CMAKE_C_COMPILER="mpicc" \
+   -D CMAKE_CXX_COMPILER="mpicxx" \
+   -D CMAKE_Fortran_COMPILER="mpif90" \
+   \
+   -D CMAKE_C_FLAGS="$BUILD_C_FLAGS" \
+   -D CMAKE_CXX_FLAGS="$BUILD_CXX_FLAGS" \
+   -D CMAKE_Fortran_FLAGS="$BUILD_F_FLAGS" \
+   -D CMAKE_EXE_LINKER_FLAGS="$BUILD_LINK_FLAGS" \
+   \
+   -D Trilinos_VERBOSE_CONFIGURE=FALSE \
+   -D Trilinos_ENABLE_ALL_PACKAGES=OFF \
+   -D Trilinos_ENABLE_SECONDARY_TESTED_CODE=OFF \
+   \
+   -D Trilinos_ENABLE_TESTS=OFF \
+   -D DART_TESTING_TIMEOUT:STRING="600" \
+   \
+   -D Trilinos_ENABLE_EXPLICIT_INSTANTIATION=ON \
+   \
+   -D Trilinos_ENABLE_OpenMP=ON \
+   -D TPL_ENABLE_Pthread=OFF \
+   \
+   -D Trilinos_ENABLE_Kokkos=ON \
+   -D Trilinos_ENABLE_KokkosCore=ON \
+   -D Kokkos_ENABLE_Serial=OFF \
+   -D Kokkos_ENABLE_OpenMP=ON \
+   -D Kokkos_ENABLE_Pthread=OFF \
+   -D TPL_ENABLE_CUDA=OFF \
+   -D Kokkos_ENABLE_Cuda=OFF \
+   -D Kokkos_ENABLE_Cuda_UVM=OFF \
+   -D KOKKOS_ENABLE_DEPRECATED_CODE=OFF \
+   \
+   -D Trilinos_ENABLE_SEACAS=ON \
+   \
+   -D TPL_ENABLE_MPI=ON \
+   \
+   -D HDF5_ROOT:PATH=${HDF5_DIR} \
+   -D HDF5_NO_SYSTEM_PATHS=ON \
+   \
+   -D PNetCDF_ROOT:PATH=${PNETCDF_DIR} \
+   \
+   -D TPL_ENABLE_Netcdf=ON \
+   -D NetCDF_ROOT:PATH=${NETCDF_DIR} \
+   \
+   -D TPL_ENABLE_CGNS=ON \
+   -D CGNS_INCLUDE_DIRS:PATH="${CGNS_DIR}/include" \
+   -D CGNS_LIBRARY_DIRS:PATH="${CGNS_DIR}/lib" \
+   -D CGNS_LIBRARY_NAMES:STRING="cgns" \
+   \
+   -D Trilinos_EXTRA_LINK_FLAGS:STRING="-lmpi" \
+   \
+   ${EXTRA_ARGS} \
+   ${TRILINOS_HOME}

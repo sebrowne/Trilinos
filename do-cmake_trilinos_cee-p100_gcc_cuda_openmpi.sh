4b32ee9f5fb54c2b6e9457cf6068efd0347253ad
Samuel Browne <sebrown@sandia.gov>
2020-07-14 08:12:54 -0600

    Add minitrilinos/seacas capabilities to main do-cmakes
    Eliminates the maintenance burden of the extra configure scripts.
    
    User Support Ticket(s) or Story Referenced: N/A

--- do-cmake_trilinos_cee-p100_gcc_cuda_openmpi.sh
+++ do-cmake_trilinos_cee-p100_gcc_cuda_openmpi.sh
@@ -28,11 +28,10 @@ BUILD_CXX_FLAGS=""
 BUILD_F_FLAGS=""
 BUILD_LINK_FLAGS=""
 BOUNDS_CHECKING=OFF
-
-if   [[ ${1} == 'opt' || ${2} == 'opt' || ${3} == 'opt' ]]
+if   [[ ${1} == 'opt' || ${2} == 'opt' || ${3} == 'opt' || ${4} == 'opt' ]]
 then
   :
-elif [[ ${1} == 'dbg' || ${2} == 'dbg' || ${3} == 'dbg' ]]
+elif [[ ${1} == 'dbg' || ${2} == 'dbg' || ${3} == 'dbg' || ${4} == 'dbg' ]]
 then
   BUILD_TYPE=DEBUG
   BUILD_SUFFIX=dbg
@@ -43,14 +42,13 @@ fi
 
 LINK_SHARED=OFF
 LINK_SUFFIX=static
-
-if   [[ ${1} == 'static' || ${2} == 'static' || ${3} == 'static' ]]
+if   [[ ${1} == 'static' || ${2} == 'static' || ${3} == 'static' || ${4} == 'static' ]]
 then
   :
   BUILD_C_FLAGS="$BUILD_C_FLAGS -fPIC"
   BUILD_CXX_FLAGS="$BUILD_CXX_FLAGS -fPIC"
   BUILD_F_FLAGS="$BUILD_F_FLAGS -fPIC"
-elif [[ ${1} == 'shared' || ${2} == 'shared' || ${3} == 'shared' ]]
+elif [[ ${1} == 'shared' || ${2} == 'shared' || ${3} == 'shared' || ${4} == 'shared' ]]
 then
   LINK_SHARED=ON
   LINK_SUFFIX=shared
@@ -60,16 +58,33 @@ fi
 
 USING_SERIAL=ON
 USING_OPENMP=OFF
-if   [[ ${1} == 'serial' || ${2} == 'serial' || ${3} == 'serial' ]]
+if   [[ ${1} == 'serial' || ${2} == 'serial' || ${3} == 'serial' || ${4} == 'serial' ]]
 then
   USING_SERIAL=ON
   USING_OPENMP=OFF
-elif [[ ${1} == 'openmp' || ${2} == 'openmp' || ${3} == 'openmp' ]]
+elif [[ ${1} == 'openmp' || ${2} == 'openmp' || ${3} == 'openmp' || ${4} == 'openmp' ]]
 then
   USING_OPENMP=OFF
   USING_SERIAL=ON
 else
   echo " *** You may specify 'serial' or 'openmp' to this configuration script. Defaulting to 'serial'!"
+fi
+
+BUILD_ALL_PACKAGES=ON
+BUILD_SEACAS=ON
+BUILD_KOKKOS=ON
+if   [[ ${1} == 'full' || ${2} == 'full' || ${3} == 'full' || ${4} == 'full' ]]
+then
+  :
+elif [[ ${1} == 'mini' || ${2} == 'mini' || ${3} == 'mini' || ${4} == 'mini' ]]
+then
+  BUILD_ALL_PACKAGES=OFF
+elif [[ ${1} == 'seacas' || ${2} == 'seacas' || ${3} == 'seacas' || ${4} == 'seacas' ]]
+then
+  BUILD_ALL_PACKAGES=OFF
+  BUILD_KOKKOS=OFF
+else
+  echo " *** You may specify 'full' or 'mini' to this configuration script. Defaulting to 'full'!"
 fi
 
 TRILINOS_HOME=${TRILINOS_REPO_DIR:-$(cd ..; pwd)}
@@ -120,49 +135,49 @@ cmake \
    -D Trilinos_ENABLE_OpenMP=OFF \
    -D TPL_ENABLE_Pthread=OFF \
    \
-   -D Trilinos_ENABLE_Teuchos=ON \
-   -D Trilinos_ENABLE_Epetra=ON \
-   -D Trilinos_ENABLE_EpetraExt=ON \
-   -D Trilinos_ENABLE_AztecOO=ON \
-   -D Trilinos_ENABLE_Amesos=ON \
-   -D Trilinos_ENABLE_Stratimikos=ON \
-   -D Trilinos_ENABLE_Anasazi=ON \
+   -D Trilinos_ENABLE_Teuchos=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Epetra=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_EpetraExt=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_AztecOO=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Amesos=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Stratimikos=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Anasazi=${BUILD_ALL_PACKAGES:?} \
    -D Anasazi_ENABLE_RBGen=OFF \
-   -D Trilinos_ENABLE_Ifpack=ON \
-   -D Trilinos_ENABLE_ML=ON \
-   -D Trilinos_ENABLE_Teko=ON \
-   -D Trilinos_ENABLE_NOX=ON \
-   -D Trilinos_ENABLE_Thyra=ON \
+   -D Trilinos_ENABLE_Ifpack=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_ML=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Teko=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_NOX=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Thyra=${BUILD_ALL_PACKAGES:?} \
    -D Trilinos_ENABLE_Rythmos=OFF \
-   -D Trilinos_ENABLE_Sacado=ON \
+   -D Trilinos_ENABLE_Sacado=${BUILD_ALL_PACKAGES:?} \
    -D Trilinos_ENABLE_Stokhos=OFF \
    -D Trilinos_ENABLE_Panzer=OFF \
-   -D Trilinos_ENABLE_Tpetra=ON \
+   -D Trilinos_ENABLE_Tpetra=${BUILD_ALL_PACKAGES:?} \
    -D Tpetra_INST_SERIAL=${USING_SERIAL:?} \
    -D Tpetra_INST_OPENMP=${USING_OPENMP:?} \
-   -D Trilinos_ENABLE_Belos=ON \
-   -D Trilinos_ENABLE_Amesos2=ON \
+   -D Trilinos_ENABLE_Belos=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_Amesos2=${BUILD_ALL_PACKAGES:?} \
    -D Amesos2_ENABLE_Epetra=OFF \
    -D Amesos2_ENABLE_KLU2=ON \
-   -D Trilinos_ENABLE_Ifpack2=ON \
-   -D Trilinos_ENABLE_MueLu=ON \
+   -D Trilinos_ENABLE_Ifpack2=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_MueLu=${BUILD_ALL_PACKAGES:?} \
    -D MueLu_ENABLE_Epetra=OFF \
    -D Xpetra_ENABLE_Epetra=OFF \
    -D Xpetra_ENABLE_EpetraExt=OFF \
-   -D Trilinos_ENABLE_Zoltan2=ON \
+   -D Trilinos_ENABLE_Zoltan2=${BUILD_ALL_PACKAGES:?} \
    -D Trilinos_ENABLE_STKMesh=OFF \
    -D Trilinos_ENABLE_STKIO=OFF \
-   -D Trilinos_ENABLE_STKTransfer=ON \
-   -D Trilinos_ENABLE_STKSearch=ON \
-   -D Trilinos_ENABLE_STKUtil=ON \
+   -D Trilinos_ENABLE_STKTransfer=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_STKSearch=${BUILD_ALL_PACKAGES:?} \
+   -D Trilinos_ENABLE_STKUtil=${BUILD_ALL_PACKAGES:?} \
    -D Trilinos_ENABLE_STKTopology=OFF \
-   -D Trilinos_ENABLE_STKSimd=ON \
+   -D Trilinos_ENABLE_STKSimd=${BUILD_ALL_PACKAGES:?} \
    -D Trilinos_ENABLE_Pamgen=OFF \
    -D Trilinos_ENABLE_Intrepid2=OFF \
    -D Trilinos_ENABLE_ShyLU_NodeHTS=OFF \
    -D Trilinos_ENABLE_ShyLU_NodeTacho=OFF \
-   -D Trilinos_ENABLE_Kokkos=ON \
-   -D Trilinos_ENABLE_KokkosCore=ON \
+   -D Trilinos_ENABLE_Kokkos=${BUILD_KOKKOS:?} \
+   -D Trilinos_ENABLE_KokkosCore=${BUILD_KOKKOS:?} \
    -D Kokkos_ENABLE_SERIAL=${USING_SERIAL:?} \
    -D Kokkos_ENABLE_OPENMP=${USING_OPENMP:?} \
    -D Kokkos_ENABLE_PTHREAD=OFF \
@@ -185,15 +200,15 @@ cmake \
    -D Teuchos_HIDE_DEPRECATED_CODE=ON\
    -D Thyra_HIDE_DEPRECATED_CODE=ON \
    \
-   -D Trilinos_ENABLE_SEACAS=ON \
+   -D Trilinos_ENABLE_SEACAS=${BUILD_SEACAS:?} \
    -D TPL_ENABLE_X11=OFF \
    -D TPL_ENABLE_Matio=OFF \
    \
-   -D Trilinos_ENABLE_Gtest=ON \
+   -D Trilinos_ENABLE_Gtest=${BUILD_ALL_PACKAGES:?} \
    \
    -D Trilinos_ENABLE_TriKota=OFF \
    -D DAKOTA_ENABLE_TESTS=OFF \
-   -D Trilinos_ENABLE_ROL=ON \
+   -D Trilinos_ENABLE_ROL=${BUILD_ALL_PACKAGES:?} \
    \
    -D TPL_ENABLE_MPI=ON \
    -D MPI_USE_COMPILER_WRAPPERS=ON \

4b291f7cb78c9cf9015f6a3cdf12bed94a60841a
Samuel Browne <sebrown@sandia.gov>
2023-06-07 13:37:56 -0600

    Tell generic driver that it's on CTS1
    For env bootstrapping.
    
    User Support Ticket(s) or Story Referenced: TRILFRAME-567

--- packages/framework/pr_tools/PullRequestLinuxCTS1.sh
+++ packages/framework/pr_tools/PullRequestLinuxCTS1.sh
@@ -19,7 +19,7 @@ set -x
 stdout=job_stdout.log
 stderr=job_stderr.log
 
-sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" "${WORKSPACE}"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh
+sbatch --output=${stdout} --error=${stderr} --wait --account=fy200165 --exclusive --partition=short --ntasks=16 --job-name="${JOB_NAME}" --time="${SLURM_CTEST_TIME_LIMIT}" "${WORKSPACE}"/Trilinos/packages/framework/pr_tools/PullRequestLinuxDriver.sh --on_cts1
 ESTAT=$?
 
 echo -e "\n\nJob stdout is as follows\n\n"

d1e0168f6cbad12f8132b84c19b39b85f2ff2679
Samuel E. Browne <sebrown@sandia.gov>
2024-10-30 08:04:57 -0600

    Find BLAS and LAPACK the "normal" way
    Meaning use the FindBLAS.cmake and FindLAPACK.cmake find modules built
    into CMake.  If they don't work, fall back to the TriBITS way.  This
    approach was taken from the TriBITS documentation at
    https://tribits.org/doc/TribitsUsersGuide.html#creating-findtpl-tplname-cmake-using-find-package-with-imported-targets
    .
    
    Signed-off-by: Samuel E. Browne <sebrown@sandia.gov>

--- cmake/TPLs/FindTPLBLAS.cmake
+++ cmake/TPLs/FindTPLBLAS.cmake
@@ -1,25 +1,41 @@
 
-if (MSVC AND NOT
-    (BLAS_LIBRARY_DIRS  OR
-     (NOT "${BLAS_LIBRARY_NAMES}" STREQUAL "blas blas_win32" AND
-      NOT "${BLAS_LIBRARY_NAMES}" STREQUAL "") OR
-     BLAS_INCLUDE_DIRS  OR
-     BLAS_INCLUDE_NAMES OR
-     (NOT "${TPL_BLAS_LIBRARIES}" STREQUAL "blas" AND
-      NOT "${TPL_BLAS_LIBRARIES}" STREQUAL "") OR
-     TPL_BLAS_INCLUDE_DIRS)
-   )
-  # Find the CLAPACK built by CMake on the machine for MSVC if found it will
-  # set the BLAS and LAPACK libraries.  NOTE: This the FindCLAPACK module must
-  # be called every configure or this does not work!
-  # If the user has specified alternate name or location of their blas that
-  # will be used instead.
-  find_package(CLAPACK 3.2.1 NO_MODULE)
-  if (CLAPACK_FOUND)
-    advanced_set(TPL_BLAS_LIBRARIES blas
-      CACHE FILEPATH "Set from MSVC CLAPACK specialization")
+tribits_tpl_allow_pre_find_package(BLAS BLAS_ALLOW_PREFIND)
+
+if (BLAS_ALLOW_PREFIND)
+  message("-- Using find_package(BLAS ...) ...")
+  find_package(BLAS)
+  if (BLAS_FOUND)
+    message("-- Found BLAS_DIR='${BLAS_DIR}'")
+    message("-- Generating BLAS::all_libs and BLASConfig.cmake")
+    tribits_extpkg_create_imported_all_libs_target_and_config_file(BLAS
+      INNER_FIND_PACKAGE_NAME  BLAS
+      IMPORTED_TARGETS_FOR_ALL_LIBS BLAS::BLAS)
   endif()
 endif()
 
-tribits_tpl_find_include_dirs_and_libraries( BLAS
-  REQUIRED_LIBS_NAMES "blas blas_win32")
+if (NOT TARGET BLAS::all_libs)
+  if (MSVC AND NOT
+      (BLAS_LIBRARY_DIRS  OR
+      (NOT "${BLAS_LIBRARY_NAMES}" STREQUAL "blas blas_win32" AND
+        NOT "${BLAS_LIBRARY_NAMES}" STREQUAL "") OR
+      BLAS_INCLUDE_DIRS  OR
+      BLAS_INCLUDE_NAMES OR
+      (NOT "${TPL_BLAS_LIBRARIES}" STREQUAL "blas" AND
+        NOT "${TPL_BLAS_LIBRARIES}" STREQUAL "") OR
+      TPL_BLAS_INCLUDE_DIRS)
+    )
+    # Find the CLAPACK built by CMake on the machine for MSVC if found it will
+    # set the BLAS and LAPACK libraries.  NOTE: This the FindCLAPACK module must
+    # be called every configure or this does not work!
+    # If the user has specified alternate name or location of their blas that
+    # will be used instead.
+    find_package(CLAPACK 3.2.1 NO_MODULE)
+    if (CLAPACK_FOUND)
+      advanced_set(TPL_BLAS_LIBRARIES blas
+        CACHE FILEPATH "Set from MSVC CLAPACK specialization")
+    endif()
+  endif()
+
+  tribits_tpl_find_include_dirs_and_libraries(BLAS
+    REQUIRED_LIBS_NAMES "blas blas_win32")
+endif()
--- cmake/TPLs/FindTPLLAPACK.cmake
+++ cmake/TPLs/FindTPLLAPACK.cmake
@@ -1,19 +1,35 @@
 
-if (MSVC AND NOT
-    (LAPACK_LIBRARY_DIRS  OR
-     (NOT "${LAPACK_LIBRARY_NAMES}" STREQUAL "lapack lapack_win32" AND
-      NOT "${LAPACK_LIBRARY_NAMES}" STREQUAL "") OR
-     LAPACK_INCLUDE_DIRS  OR
-     LAPACK_INCLUDE_NAMES OR
-     (NOT "${TPL_LAPACK_LIBRARIES}" STREQUAL "lapack" AND
-      NOT "${TPL_LAPACK_LIBRARIES}" STREQUAL "") OR
-     TPL_LAPACK_INCLUDE_DIRS)
-   )
-  if(CLAPACK_FOUND)
-    advanced_set(TPL_LAPACK_LIBRARIES lapack
-        CACHE FILEPATH "Set from MSVC CLAPACK specialization")
+tribits_tpl_allow_pre_find_package(LAPACK LAPACK_ALLOW_PREFIND)
+
+if (LAPACK_ALLOW_PREFIND)
+  message("-- Using find_package(LAPACK ...) ...")
+  find_package(LAPACK)
+  if (LAPACK_FOUND)
+    message("-- Found LAPACK_DIR='${LAPACK_DIR}'")
+    message("-- Generating LAPACK::all_libs and LAPACKConfig.cmake")
+    tribits_extpkg_create_imported_all_libs_target_and_config_file(LAPACK
+      INNER_FIND_PACKAGE_NAME  LAPACK
+      IMPORTED_TARGETS_FOR_ALL_LIBS LAPACK::LAPACK)
   endif()
 endif()
 
-tribits_tpl_find_include_dirs_and_libraries( LAPACK
-  REQUIRED_LIBS_NAMES "lapack lapack_win32")
+if (NOT TARGET LAPACK::all_libs)
+  if (MSVC AND NOT
+      (LAPACK_LIBRARY_DIRS  OR
+      (NOT "${LAPACK_LIBRARY_NAMES}" STREQUAL "lapack lapack_win32" AND
+        NOT "${LAPACK_LIBRARY_NAMES}" STREQUAL "") OR
+      LAPACK_INCLUDE_DIRS  OR
+      LAPACK_INCLUDE_NAMES OR
+      (NOT "${TPL_LAPACK_LIBRARIES}" STREQUAL "lapack" AND
+        NOT "${TPL_LAPACK_LIBRARIES}" STREQUAL "") OR
+      TPL_LAPACK_INCLUDE_DIRS)
+    )
+    if(CLAPACK_FOUND)
+      advanced_set(TPL_LAPACK_LIBRARIES lapack
+          CACHE FILEPATH "Set from MSVC CLAPACK specialization")
+    endif()
+  endif()
+
+  tribits_tpl_find_include_dirs_and_libraries(LAPACK
+    REQUIRED_LIBS_NAMES "lapack lapack_win32")
+endif()
